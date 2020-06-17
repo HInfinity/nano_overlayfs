635a31ddb8fc8f0fe697f9871dacc5efa0a12cea
Your Name <you@example.com>
2020-06-18 07:21:49 +0800

    Update script

--- convert_to_rofs.sh
+++ convert_to_rofs.sh
@@ -3,17 +3,26 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-sudo -i # root shell
-sudo cp ./hooks/root-ro /etc/initramfs-tools/hooks/root-ro
-sudo cp ./scripts/root-ro /etc/initramfs-tools/scripts/init-bottom/root-ro
+
+echo -e "\e[47m \e[32m Copy hooks"
+echo -e "\e[49m \e[39m .."
+cp ./hooks/root-ro /etc/initramfs-tools/hooks/root-ro
+cp ./scripts/root-ro /etc/initramfs-tools/scripts/init-bottom/root-ro
+echo -e "\e[47m \e[32m Mod hooks"
+echo -e "\e[49m \e[39m .."
 chmod +x /etc/initramfs-tools/hooks/root-ro
 chmod +x /etc/initramfs-tools/scripts/init-bottom/root-ro
 # Update the initrd file
+echo -e "\e[47m \e[32m Update FS"
+echo -e "\e[49m \e[39m .."
 update-initramfs -u
 if ! grep -q "INITRD /boot/initrd.img" /boot/extlinux/extlinux.conf; then sed -i "s/INITRD \/boot\/initrd/INITRD \/boot\/initrd.img/" /boot/extlinux/extlinux.conf; fi
-sudo cp ./scripts/reboot-ro /root/reboot-ro
-sudo cp ./scripts/reboot-rw /root/reboot-rw
+echo -e "\e[47m \e[32m Convenience scripts"
+echo -e "\e[49m \e[39m .."
+cp ./scripts/reboot-ro /root/reboot-ro
+cp ./scripts/reboot-rw /root/reboot-rw
 chmod +x /root/reboot-ro
 chmod +x /root/reboot-rw
-exit # exit root shell
+echo -e "\e[47m \e[32m ROFS install complete. use sudo reboot-ro to start in read only mode"
+
 
